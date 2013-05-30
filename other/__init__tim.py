0b55550aa03e5750068ae3009580de8a73ec3879
wj2 <williamjjohnston@gmail.com>
2013-05-30 18:35:39 -0400

    bug fixes and gui update, removed extraneous code

--- other/__init__tim.py
+++
@@ -1,5 +0,0 @@
-"""
-NeuroPy settings for user tim
-"""
-datapath = os.path.join(os.sep,'Users','tim','data')
-cachepath = os.path.join(os.sep,'Users','tim','data','cache')
--- ptarg.py
+++ ptarg.py
@@ -10,12 +10,12 @@ from scipy.signal import argrelmax
 # replace this section with a few lines
 import vasc.vastifarr as va
 
-from config import path
+from config import path, probesize
 
 a = va.tiffile(path)
 tiflist = a.gettiflist(path)
 tiflist2, xml = a.get_tiffs_and_xml(tiflist)
-# tiflist2 = tiflist2[50:75]
+tiflist2 = tiflist2[50:75]
 nfiles = a.listsize(tiflist2)
 array3d = a.initarray(nfiles)
 tif3darray = a.tif2array(path,tiflist2,array3d)
@@ -25,25 +25,42 @@ meanvasdam = np.mean(tif3darray,axis=0)
 
 def mousemove(event):
     # plot best orientation at a given location
-    if event.inaxes == ax2 or event.inaxes == ax3:
-        img.set_data(probe[np.argmin(damage[:,event.xdata,event.ydata])])
-        extent = [event.xdata - kxy/2, event.xdata + kxy/2, event.ydata - kxy/2, event.ydata + kxy/2]
+    if event.inaxes == ax1 or event.inaxes == ax2:
+        i = np.argmin(damage[:,event.ydata,event.xdata])
+        img.set_data(probe[i])
+        extent = [event.xdata - kxy/2, 
+                  event.xdata + kxy/2, 
+                  event.ydata - kxy/2, 
+                  event.ydata + kxy/2]
         img.set_extent(extent)
-        plt.ylim(512,0)
-        plt.xlim(0,512)
-        # subplot(122)
-        # plt.title('damage=%1d'%np.min(damage[:,event.xdata,event.ydata]))
+        ax2.set_ylim(512,0)
+        ax2.set_xlim(0,512)
         print('damage=%1d'%np.min(damage[:,event.ydata,event.xdata]))
+        
+        ax3.clear()
+        ax3.plot(lineprof[0, i, event.ydata, event.xdata])
+
+        plt.draw()
+    elif event.inaxes == ax4:
+        i = np.argmin(damage[:,event.ydata,event.xdata])
+        img.set_data(probe[i])
+        extent = [event.xdata - kxy/2, 
+                  event.xdata + kxy/2, 
+                  event.ydata - kxy/2, 
+                  event.ydata + kxy/2]
+        img.set_extent(extent)
+        ax2.set_ylim(512,0)
+        ax2.set_xlim(0,512)
+        print('damage=%1d'%np.min(lumdam[:,event.ydata,event.xdata]))
+
         plt.draw()
 
-def micron2pix(probsize):       # conversion from microns to pixels - remove hardcoding, get from Prairie XML metadata file
+
+def micron2pix(probsize):       
     scales = a.get_scale(xml)
     s = (1 / scales[0], 1 / scales[1])
     newprobsize = [int(probsize[0]*s[0]),int(probsize[1]*s[1])]
     return newprobsize
-
-# probesz = [70,23]
-probesz = [32,23]
 
 def build_kernel(probesizemicron, theta = [0]):
     # make mask of probe cross-section
@@ -84,16 +101,17 @@ def subz_from_3dtiff(tiffs, slicesize):
 
 def count_vessels(probesizemicron, theta = [0]):
     # algorithm pseudocode:
-    # rotate whole *image* not probe #
+    # normalize image slice
+    # rotate probe
     # slice rotated image to generate line profiles 
     # low pass filter line profiles to remove noise
     # find peaks of line profile (local maxima)
-    # discard peaks below ~500 lum. units 
-    # count the number of peaks --> number of crossed vessels at that location
+    # discard peaks below empirical threshold  
+    # count the number of peaks --> number of crossed vessels at  location
     # no need to adjust discard theshold with depth
     # repeat above for all substacks in the whole imaging volume
     # p.s. tested on 50um substacks
-
+    thresh = -0.9
     # create sub-z tiff stack
     subzstack = subz_from_3dtiff(tif3darray, 25)
     print tif3darray.shape
@@ -106,6 +124,8 @@ def count_vessels(probesizemicron, theta = [0]):
     k[l/2] = 1 # horizontal probe
     damage2d = np.zeros((len(theta),meanvasdam.shape[0],
                          meanvasdam.shape[1]))
+    lineprofiles = np.zeros((subzstack.shape[0], len(theta), meanvasdam.shape[0],
+                             meanvasdam.shape[1]), dtype=object)
     for lnum,layer in enumerate(subzstack):
         print 'layer '+str(lnum+1)+' of '+str(subzstack.shape[0])
         layer = dsp.normalize(layer)
@@ -118,28 +138,31 @@ def count_vessels(probesizemicron, theta = [0]):
             rotated[rotated < rotated.max()] = 0
             rotated[rotated == rotated.max()] = 1
             # for y in xrange(layer.shape[0]+l): # add step here...
-            for y in xrange(layer.shape[0]/2-100, layer.shape[0]/2+100): # add step here...
+            for y in xrange(layer.shape[0]/2-100, layer.shape[0]/2+100):
                 # for x in xrange(layer.shape[1]-l): # add step here...
-                for x in xrange(layer.shape[1]/2-100, layer.shape[1]/2+100): # add step here...
+                for x in xrange(layer.shape[1]/2-100, layer.shape[1]/2+100): 
                     # print lnum, x, y
                     a=rotated*layer[y:y+l,x:x+l]
                     b=a.T[a.T.nonzero()]
                     # luminance est of collision
                     if b.mean() > 0:
                         damage2d[i,y,x] += 1
-                    profl = dsp.smoothg(b,10) # smoothed profile for this location
-                    profl[profl < -0.9] = 0 # discard small peaks
+                    # profl = b
+                    profl = dsp.smoothg(b,9) # smoothed profile for this location
+                    profl[profl < thresh] = thresh # discard small peaks
                     
-                    # damage2d[i,y,x] += sum(dsp.islocmax(profl)) # count peaks --> vessels
-                    damage2d[i,y,x] += argrelmax(profl, order=2)[0].size # count peaks --> vessels
-    return damage2d
-            
+                    lineprofiles[lnum, i, y, x] = profl
 
+                    # count peaks --> vessels
+                    # damage2d[i,y,x] += sum(dsp.islocmax(profl))
+                    damage2d[i,y,x] += argrelmax(profl, order=1)[0].size 
+
+    return damage2d, lineprofiles
 
 ori = np.array([0,30,60,90,120,150])
-# damage = build_histogram(probesz, ori)
-damage = count_vessels(probesz, ori)
-probe = build_kernel(probesz, ori)
+damage, lineprof = count_vessels(probesize, ori)
+lumdam = build_histogram(probesize, ori)
+probe = build_kernel(probesize, ori)
 kxy = probe.shape[1]
 
 # plot vascular pattern with optimal locations overlaid
@@ -155,16 +178,25 @@ fig.canvas.mpl_connect('motion_notify_event',mousemove)
 # plt.legend()
 # 
 # plot the estimated damage for the least damaging orientation
-ax2=plt.subplot(121)
-plt.imshow(damage.min(0))
-plt.title('Vascular damage at best orientation')
-plt.axis('off')
+ax1 = fig.add_subplot(221)
+ax1.imshow(damage.min(0))
+ax1.set_title('Vascular damage at best orientation')
 
 # plot the interactive GUI
-ax3=plt.subplot(122)
-plt.imshow(meanvasdam,'gray',interpolation='none')
+ax2 = plt.subplot(222)
+ax2.imshow(meanvasdam,'gray',interpolation='none')
+
+# plot probe
 img = plt.imshow(probe[0], extent = [0,0,0,0], alpha = 0.5)
-plt.axis('off')
+
+# plot dynamic timecourse
+ax3 = fig.add_subplot(223)
+ax3.plot([1,2,3,4,5])
+
+# plot luminance
+ax4 = fig.add_subplot(224)
+ax4.imshow(lumdam.min(0))
+
 plt.draw()
 plt.show()
 plt.ioff()
