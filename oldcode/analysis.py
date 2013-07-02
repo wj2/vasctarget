20ab13ef3e234bbed63982b6cc943cea923d37dd
wj2 <williamjjohnston@gmail.com>
2013-07-02 15:17:57 -0400

    actually did what I said before

--- oldcode/analysis.py
+++ oldcode/analysis.py
@@ -1,30 +1,78 @@
 
-import sys
+import sys, csv, time
 
 import numpy as np
 
-from proc import normalize, smoothg, clean_rotate
+from proc import *
 from scipy.misc import imrotate
 from scipy.signal import argrelmax
+from scipy.ndimage import gaussian_filter
+from scipy.stats.mstats import kurtosis
+from scipy.stats import skew
 
+def print_damage_stats(pre, post, masks):
+    t = time.strftime('%X%x%Z').replace(' ','').replace(':','.').replace('/','-')
+    filename = 'damage'+t+'.csv'
+    with open(filename, 'wb') as cf:
+        writ = csv.writer(cf)
+        
+        writ.writerow([pre[0]]+['' for x in xrange(len(masks) - 1)]
+                      +[post[0]]+['' for x in xrange(len(masks) - 1)])
+        writ.writerow([m.shape for m in masks] + [m.shape for m in masks])
+        for i,m in enumerate(pre[1]):
+            writ.writerow(list(pre[1][i])+list(post[1][i]))
+            
+    return None
 
-def profile(section, probe, thresh=-0.9):
-    dam = 0
-    p = probe * section
-    p = p.T[p.T.nonzero()]
-    # if avg luminance is high (eg, on a vessel)
-    if p.mean() > 0: 
-        dam += 1
+def print_misc_stats(path, alldata, key, write=True): 
+    name = path.replace('/', '-').replace('.','l') + '_' + key + '_stats'
+    want = [np.mean, np.median, np.min, np.max, skew, kurtosis]
+    names, results = ['data'], [path]
+    for x in want:
+        names.append(x.__name__)
+        results.append(x(alldata[key], axis=None))
+        
+    if write:
+        with open(name+'.csv', 'wb') as cf:
+            writ = csv.writer(cf)
+            writ.writerow(names)
+            writ.writerow(results)
+        return None
+    else:
+        return names, results
+
+def print_tddict(dic):
+    t = time.strftime('%X%x%Z').replace(' ','').replace(':','.').replace('/','-')
+    filename = 'stats'+t+'.csv'
     
-    p = smoothg(p, 9) # smooth 
+    with open(filename, 'wb') as cf:
+        writ = csv.writer(cf)
+        writ.writerow(dic['title'])
+        for l in dic['data']:
+            writ.writerow(l)
+    
+
+def profile(p, thresh=-0.7, upper=1.5, smooth=20, peaksize=5):
+    dam = 0
+    # if avg luminance is high (eg, on a vessel)
+    # if pmean > 0.5: 
+    #     dam += 1
+    #     p[p > 1.5] = 1
+    
+    p = smoothg(p, smooth) # smooth 
+    t = p > upper
+    dam += np.diff(t).nonzero()[0][::2].size
+    p[p > upper] = upper
+
     p[p < thresh] = thresh # elim small peaks
 
     # find number of peaks (ie, local maxima)
-    dam += argrelmax(p, order=1)[0].size
-    
+    dam += argrelmax(p, order=peaksize)[0].size
+
     return dam, p
         
-def line_profiles(stack, thetas, down, probesize):
+def line_profiles(views, stack, thetas, down, gauss, probesize):
+    # avoid big top level vessels, how to do it visually
     # represent probe as single line
     l = probesize[0]
     probe = np.zeros((l, l))
@@ -40,7 +88,7 @@ def line_profiles(stack, thetas, down, probesize):
                              (stack.shape[1] - l) / down + 1,
                              (stack.shape[2] - l) / down + 1), 
                             dtype=object)
-    
+
     # begin loop procedure
     for lnum, layer in enumerate(stack):
         sys.stdout.write('layer '+str(lnum+1)+' of '+str(stack.shape[0])+'\n')
@@ -53,16 +101,24 @@ def line_profiles(stack, thetas, down, probesize):
                 xs = 0
                 for x in xrange(0, layer.shape[1] - l + 1, down):
                     # do line profile
-                    dam, line = profile(layer[y:y+l, x:x+l],
-                                        rotated)
+                    section = layer[y:y+l, x:x+l]
+                    p = rotated * section
+                    p = p.T[p.T.nonzero()]
+
+                    dam, line = profile(p)
                     damage[i, ys, xs] += dam
                     lineprofiles[lnum, i, ys, xs] = line
                     xs += 1
                 ys += 1
 
-    return damage, lineprofiles
+    views['line'] = damage
+    views['lineprof'] = lineprofiles
+    if gauss:
+        smoothdamage = gaussian_filter(damage, 1)
+        views['linegauss'] = np.ceil(smoothdamage)
+    return views
 
-def luminance(stack, thetas, probesize):
+def luminance(views, stack, thetas, probesize):
     # construct probe representation
     probe = make_probe(probesize)
     # container for lumdamage
@@ -73,5 +129,26 @@ def luminance(stack, thetas, probesize):
             rotated = clean_rotate(probe, t, interp='nearest')
             damage[lnum, i] = ndimage.convolve(layer, rotated)
 
-    return damage.sum(axis=0)
+    views['luminance'] = damage.sum(axis=0)
+    return views
+    
+def damage_profiles(stack, locs, rots, inter, n, psize):
+    masks = create_masks(psize, inter, n)
+
+    count = np.zeros((stack.shape[0], n + 1))
+    lines = np.empty((stack.shape[0], n + 1), dtype=object)
+    for i, layer in enumerate(stack):
+        rotlayer, yx = rotate_mask_horizontal(layer, rots[i], locs[i])
+        rotlayer = normalize(rotlayer)
+        print rotlayer.shape, yx
+        for j, mask in enumerate(masks):         
+            print mask.shape
+            section = rotlayer[yx[0]:yx[0]+mask.shape[0],
+                               yx[1]:yx[1]+mask.shape[1]]
+            dat = mask * section
+            prof = collapse_rect_mask(dat)
+        
+            count[i, j], lines[i, j] = profile(prof)
+
+    return count, lines, masks
     
